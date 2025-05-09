4f0e579fcd6c75a58104937c2683ce37c9e664db
Krenk <67797667+Krenk-lab@users.noreply.github.com>
2025-05-09 13:56:43 +0500

    Add code
    Добавил код для шарика

---
+++ Sharik_2.0/Assets/Scripts/Balloon/Move.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(0, 5);
+    }
+}
